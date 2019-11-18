0ebd7a72bf59f506b4a94385336fcb1b4e85103b
Sam Wilkins <samwilkins333@gmail.com>
2019-11-18 12:20:44 -0500

    initial commit

---
+++ build/precache-manifest.93adf3a2ecd895561f2a56c3bb91e6c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c41e8aa0fa349c2190be9645f9a360",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9814bf402afc21fca0cf",
+    "url": "/static/css/main.1d7fabf1.chunk.css"
+  },
+  {
+    "revision": "1720a4f435d6b59a1dbd",
+    "url": "/static/js/2.479128d1.chunk.js"
+  },
+  {
+    "revision": "9814bf402afc21fca0cf",
+    "url": "/static/js/main.9b80c2d5.chunk.js"
+  },
+  {
+    "revision": "a2f72eae3fdd47a76cf7",
+    "url": "/static/js/runtime-main.98a606d5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ build/static/js/content_scripts/index.js
@@ -0,0 +1,3 @@
+(function showTitle() {
+    console.log(document);
+})();
